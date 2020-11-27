43f1487489d23fd5c7379b47d3900306a0ac8015
Marcus Strandner <marcus@strandner.se>
2020-11-27 23:39:55 +0100

    Extensions

---
+++ Twitch.Net.Client/Client/Extensions/EmoteModeExtension.cs
@@ -0,0 +1,27 @@
+﻿using Twitch.Net.Client.Models;
+
+namespace Twitch.Net.Client.Client.Extensions
+{
+    public static class EmoteModeExtension
+    {
+        public static void EmoteOnlyOn(
+            this IIrcClient client,
+            ChatChannel channel)
+            => client.SendMessage(channel, "/emoteonly");
+
+        public static void EmoteOnlyOn(
+            this IIrcClient client,
+            string channel)
+            => client.SendMessage(channel, "/emoteonly");
+        
+        public static void EmoteOnlyOff(
+            this IIrcClient client,
+            ChatChannel channel)
+            => client.SendMessage(channel, "/emoteonlyoff");
+
+        public static void EmoteOnlyOff(
+            this IIrcClient client,
+            string channel)
+            => client.SendMessage(channel, "/emoteonlyoff");
+    }
+}
---
+++ Twitch.Net.Client/Client/Extensions/TimeoutUserExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Twitch.Net.Client.Models;
+
+namespace Twitch.Net.Client.Client.Extensions
+{
+    public static class TimeoutUserExtension
+    {
+        private const int MaxTimeOutTime = 1_209_600;
+        
+        public static void TimeoutUser(
+            this IIrcClient client,
+            ChatChannel channel,
+            string user,
+            TimeSpan duration,
+            string reason)
+            => client.SendMessage(channel, TimeoutCommand(user, duration, reason));
+
+        public static void TimeoutUser(
+            this IIrcClient client,
+            string channel,
+            string user,
+            TimeSpan duration,
+            string reason)
+            => client.SendMessage(channel, TimeoutCommand(user, duration, reason));
+
+        private static string TimeoutCommand(string user, TimeSpan duration, string reason)
+        {
+            if (duration.TotalSeconds > MaxTimeOutTime)
+                duration = new TimeSpan(0, 0, MaxTimeOutTime);
+            else if (duration.TotalSeconds < 1)
+                duration = new TimeSpan(0, 0, 1);
+            return $"/timeout {user} {duration.TotalSeconds} {reason}";
+        }
+    }
+}
