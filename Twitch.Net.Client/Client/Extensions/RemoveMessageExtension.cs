2a4abdbea53d7f2b890e301431e6988c178e8770
Marcus Strandner <marcus@strandner.se>
2021-05-30 12:31:57 +0200

    Wrong method name & file was tab ind instead of spaces.

--- Twitch.Net.Client/Client/Extensions/RemoveMessageExtension.cs
+++ Twitch.Net.Client/Client/Extensions/RemoveMessageExtension.cs
@@ -2,23 +2,23 @@
 
 namespace Twitch.Net.Client.Client.Extensions
 {
-	public static class RemoveMessageExtension
-	{
-		public static void TimeoutUser(
-			this IIrcClient client,
-			ChatChannel channel,
-			string messageId
-			)
-			=> client.SendMessage(channel, RemoveMessageCommand(messageId));
+    public static class RemoveMessageExtension
+    {
+        public static void RemoveMessage(
+            this IIrcClient client,
+            ChatChannel channel,
+            string messageId
+            )
+            => client.SendMessage(channel, RemoveMessageCommand(messageId));
 
-		public static void TimeoutUser(
-			this IIrcClient client,
-			string channel,
-			string messageId
-			)
-			=> client.SendMessage(channel, RemoveMessageCommand(messageId));
+        public static void RemoveMessage(
+            this IIrcClient client,
+            string channel,
+            string messageId
+            )
+            => client.SendMessage(channel, RemoveMessageCommand(messageId));
 		
-		private static string RemoveMessageCommand(string messageId) =>
-			$"/delete {messageId}";
-	}
+        private static string RemoveMessageCommand(string messageId) =>
+            $"/delete {messageId}";
+    }
 }
